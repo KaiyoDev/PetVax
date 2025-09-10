3ddb8dee7cf80f197fdd7bf415f7ced939ac41a5
huynhcongduy2486 <huynhcongduy2408@gmail.com>
2025-09-10 22:15:26 +0700

    Update __init__.py

--- server/app/routes/__init__.py
+++ server/app/routes/__init__.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter
-router = APIRouter()
-
-@router.get("/pets")
-def get_pets():
-    return [{"id": 1, "name": "Vang"}, {"id": 2, "name": "Den"}]
-
-@router.post("/pets")
-def add_pet(pet: dict):
-    return {"message": "Pet added successfully", "pet": pet}
+from .pet_routes import router as pet_router
+from .vaccine_routes import router as vaccine_router
+api_router = APIRouter()
+api_router.include_router(pet_router)
+api_router.include_router(vaccine_router)
